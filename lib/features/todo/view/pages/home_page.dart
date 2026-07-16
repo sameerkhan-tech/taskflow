5016898472ffed262945e66159fb19bb839c37c1
Sameer khan <skhandeshwali01@gmail.com>
2026-07-16 17:00:28 +0530

    refactor: extract HomePage UI into helper methods

--- lib/features/todo/view/pages/home_page.dart
+++ lib/features/todo/view/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+import 'package:taskflow/features/todo/model/todo_model.dart';
 import 'package:taskflow/features/todo/model/todo_priority.dart';
 import 'package:taskflow/features/todo/view_model/todo_bloc.dart';
 
@@ -12,7 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final dateFormater = DateFormat('EEE, d MMM');
+  final _dateFormater = DateFormat('EEE, d MMM');
   @override
   void initState() {
     context.read<TodoBloc>().add(FetchTodos());
@@ -28,9 +29,10 @@ class _HomePageState extends State<HomePage> {
           return CustomScrollView(
             slivers: [
               SliverAppBar(
-                title: Text(greetingMessage()),
+                title: Text(_greetingMessage()),
                 titleTextStyle: theme.textTheme.titleMedium,
               ),
+
               // Initial loader
               if (state.isInitialLoading)
                 const SliverFillRemaining(
@@ -43,39 +45,7 @@ class _HomePageState extends State<HomePage> {
                 )
               // Error state UI
               else if (state.hasError)
-                SliverFillRemaining(
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text(
-                            state.errorMessage!,
-                            textAlign: TextAlign.center,
-                            style: theme.textTheme.bodyMedium?.copyWith(
-                              color: theme.colorScheme.error,
-                            ),
-                          ),
-                        ),
-                        FilledButton(
-                          style: FilledButton.styleFrom(
-                            backgroundColor: theme.colorScheme.errorContainer,
-                          ),
-                          onPressed: () {
-                            context.read<TodoBloc>().add(FetchTodos());
-                          },
-                          child: Text(
-                            'Retry',
-                            style: theme.textTheme.labelLarge?.copyWith(
-                              color: theme.colorScheme.error,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
+                SliverFillRemaining(child: _errorUi(state, theme, context))
               // List UI
               else
                 SliverPadding(
@@ -84,137 +54,7 @@ class _HomePageState extends State<HomePage> {
                     itemCount: state.data.length,
                     itemBuilder: (context, index) {
                       final todo = state.data[index];
-                      return Card(
-                        child: ListTile(
-                          contentPadding: EdgeInsets.symmetric(
-                            horizontal: 0,
-                            vertical: 8,
-                          ),
-                          leading: Checkbox(
-                            value: todo.isCompleted,
-                            onChanged: (value) {},
-                            shape: CircleBorder(),
-                          ),
-                          title: Text(
-                            todo.todo,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: theme.textTheme.bodyMedium?.copyWith(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              if (todo.description != null)
-                                Text(
-                                  todo.description!,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: theme.textTheme.bodySmall?.copyWith(
-                                    color: Colors.grey.shade600,
-                                  ),
-                                ),
-                              SizedBox(height: 4),
-                              Wrap(
-                                runSpacing: 4,
-                                spacing: 4,
-                                children: [
-                                  customChip(
-                                    theme: theme,
-                                    child: Text(
-                                      todo.category ?? 'General',
-                                      style: theme.textTheme.labelSmall,
-                                    ),
-                                  ),
-                                  customChip(
-                                    theme: theme,
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        if (todo.priority == TodoPriority.low)
-                                          Icon(Icons.arrow_downward, size: 14)
-                                        else if (todo.priority ==
-                                            TodoPriority.medium)
-                                          Icon(
-                                            Icons.linear_scale_outlined,
-                                            size: 14,
-                                          )
-                                        else if (todo.priority ==
-                                            TodoPriority.high)
-                                          Icon(Icons.arrow_upward, size: 14)
-                                        else
-                                          Icon(
-                                            Icons.star,
-                                            color: Colors.redAccent,
-                                            size: 14,
-                                          ),
-
-                                        Text(
-                                          (TodoPriority
-                                                  .values[todo.priority.index]
-                                                  .name)
-                                              .toUpperCase(),
-                                          style: theme.textTheme.labelSmall,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-
-                                  if (todo.dueDate != null)
-                                    customChip(
-                                      theme: theme,
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          Icon(
-                                            Icons.calendar_month,
-                                            size: 14,
-                                            color: Colors.redAccent,
-                                          ),
-                                          Text(
-                                            dateFormater.format(todo.dueDate!),
-                                            style: theme.textTheme.labelSmall
-                                                ?.copyWith(
-                                                  color: Colors.redAccent,
-                                                ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  if (todo.reminderDate != null)
-                                    customChip(
-                                      theme: theme,
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          Icon(
-                                            Icons.alarm_rounded,
-                                            size: 14,
-                                            color: Colors.blueAccent,
-                                          ),
-                                          Text(
-                                            dateFormater.format(
-                                              todo.reminderDate!,
-                                            ),
-                                            style: theme.textTheme.labelSmall
-                                                ?.copyWith(
-                                                  color: Colors.blueAccent,
-                                                ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                ],
-                              ),
-                            ],
-                          ),
-                          trailing: IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.edit),
-                          ),
-                        ),
-                      );
+                      return _todoCardUi(todo, theme);
                     },
                   ),
                 ),
@@ -225,7 +65,138 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Container customChip({required ThemeData theme, required Widget child}) {
+  Center _errorUi(TodoState state, ThemeData theme, BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              state.errorMessage!,
+              textAlign: TextAlign.center,
+              style: theme.textTheme.bodyMedium?.copyWith(
+                color: theme.colorScheme.error,
+              ),
+            ),
+          ),
+          FilledButton(
+            style: FilledButton.styleFrom(
+              backgroundColor: theme.colorScheme.errorContainer,
+            ),
+            onPressed: () {
+              context.read<TodoBloc>().add(FetchTodos());
+            },
+            child: Text(
+              'Retry',
+              style: theme.textTheme.labelLarge?.copyWith(
+                color: theme.colorScheme.error,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Card _todoCardUi(TodoModel todo, ThemeData theme) {
+    return Card(
+      child: ListTile(
+        contentPadding: EdgeInsets.symmetric(horizontal: 0, vertical: 8),
+        leading: Checkbox(
+          value: todo.isCompleted,
+          onChanged: (value) {},
+          shape: CircleBorder(),
+        ),
+        title: Text(
+          todo.todo,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          style: theme.textTheme.bodyMedium?.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (todo.description != null)
+              Text(
+                todo.description!,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: theme.textTheme.bodySmall?.copyWith(
+                  color: Colors.grey.shade600,
+                ),
+              ),
+            SizedBox(height: 4),
+            Wrap(
+              runSpacing: 4,
+              spacing: 4,
+              children: [
+                _chip(
+                  theme: theme,
+                  child: Text(
+                    todo.category ?? 'General',
+                    style: theme.textTheme.labelSmall,
+                  ),
+                ),
+
+                _chip(
+                  theme: theme,
+                  child: _buildPriorityChipContent(theme, todo),
+                ),
+
+                if (todo.dueDate != null)
+                  _iconChip(
+                    theme,
+                    label: _dateFormater.format(todo.dueDate!),
+                    icon: Icons.calendar_month,
+                    foregroundColor: Colors.redAccent,
+                  ),
+                if (todo.reminderDate != null)
+                  _iconChip(
+                    theme,
+                    label: _dateFormater.format(todo.reminderDate!),
+                    icon: Icons.alarm,
+                    foregroundColor: Colors.blueAccent,
+                  ),
+              ],
+            ),
+          ],
+        ),
+        trailing: IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+      ),
+    );
+  }
+
+  Widget _buildPriorityChipContent(ThemeData theme, TodoModel todo) {
+    final IconData iconData = switch (todo.priority) {
+      TodoPriority.low => Icons.arrow_downward,
+      TodoPriority.medium => Icons.remove,
+      TodoPriority.high => Icons.arrow_upward,
+      TodoPriority.urgent => Icons.star,
+    };
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(
+          iconData,
+          size: 14,
+          color: (todo.priority == TodoPriority.urgent)
+              ? Colors.redAccent
+              : null,
+        ),
+        const SizedBox(width: 4),
+        Text(
+          TodoPriority.values[todo.priority.index].name.toUpperCase(),
+          style: theme.textTheme.labelSmall,
+        ),
+      ],
+    );
+  }
+
+  Container _chip({required ThemeData theme, required Widget child}) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
       decoration: BoxDecoration(
@@ -235,20 +206,37 @@ class _HomePageState extends State<HomePage> {
       child: child,
     );
   }
-}
 
-String greetingMessage() {
-  final int hour = DateTime.now().hour;
-
-  if (hour < 5) {
-    return "Hello, Night Owl 🌙";
-  } else if (hour > 5 && hour < 12) {
-    return "Hello, Good morning 🌄";
-  } else if (hour > 12 && hour < 17) {
-    return "Hello, Good afternoon ☀️";
-  } else if (hour > 17 && hour < 21) {
-    return "Hello, Good evening 🌇";
-  } else {
-    return "Hello, Working late? 🌛";
+  Widget _iconChip(
+    ThemeData theme, {
+    required String label,
+    required IconData icon,
+    required Color foregroundColor,
+  }) {
+    return _chip(
+      theme: theme,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(icon, size: 14, color: foregroundColor),
+          const SizedBox(width: 4),
+          Text(
+            label,
+            style: theme.textTheme.labelSmall?.copyWith(color: foregroundColor),
+          ),
+        ],
+      ),
+    );
   }
+
+ String _greetingMessage() {
+  final hour = DateTime.now().hour;
+
+  if (hour < 5) return "Hello, Night Owl 🌙";
+  if (hour < 12) return "Hello, Good morning 🌄";
+  if (hour < 17) return "Hello, Good afternoon ☀️";
+  if (hour < 21) return "Hello, Good evening 🌇";
+
+  return "Hello, Working late? 🌛";
+}
 }
